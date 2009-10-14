4a018fe055f13922c5625ab08fa46652933c04d9
Leonid Bugaev <leonsbox@gmail.com>
2009-10-15 00:16:18 +0400

    Some changes

--- src/run.sh
+++ src/run.sh
@@ -1,1 +1,1 @@
-python2.5 /home/buger/work/google_appengine/dev_appserver.py /home/buger/workspace/flickr/src/
+python2.5 /home/buger/work/google_appengine/dev_appserver.py /home/buger/workspace/FlickFaver/src/
